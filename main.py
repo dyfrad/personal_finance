4695852165d034506680b15fbb708806ef9992e2
Mohit Saharan <mohit@msaharan.com>
2025-06-20 22:57:01 +0200

    refactor: remove hardcoded author and version from main.py docstring

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-Author: Mohit Saharan
-Version: 0.2.0
 """
 
 import sys
