3ebfa1a5c79193fd7839c9df6edbacf909502405
Mohit Saharan <mohit@msaharan.com>
2025-06-24 15:11:03 +0200

    Minimize module header docstrings

--- gui.py
+++ gui.py
@@ -1,23 +1,4 @@
-"""
-Personal Finance Manager - GUI Application
-
-A Python-based personal finance management application that helps you track investments, household inventory, and expenses.
-
-Copyright (c) 2025 Mohit Saharan
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
+"""Graphical user interface implementation with tkinter."""
 
 import tkinter as tk
 from tkinter import ttk, messagebox
